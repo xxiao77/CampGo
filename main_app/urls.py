77f1ce042b29227c9efc460ee0787333706c0036
Ryan Dennis <rrdennis@gmail.com>
2020-02-06 13:53:08 -0800

    First commit

---
+++ main_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+  path('accounts/signup/', views.signup, name='signup'),
+  path('', views.home, name='home'),
+  path('campgo/', views.index, name='index'),
+  path('about/', views.about, name='about')
+]
+  
