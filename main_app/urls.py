807aa51fee673c2ef28474cf2e1a82beeae4f951
killerwhiteshark <killerwhiteshark@users.noreply.github.com>
2020-02-06 16:27:49 -0800

    Add campsite HTML pages Add, Edit, Confrim Delete and Routes

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
   path('accounts/signup/', views.signup, name='signup'),
   path('', views.home, name='home'),
   path('campgo/', views.index, name='index'),
-  path('about/', views.about, name='about')
+  path('about/', views.about, name='about'),
+  path('camp_create/', views.camp_create, name='camp_create'),
+  path('camp_edit/', views.camp_edit, name='camp_edit'),
+  path('camp_delete/', views.camp_delete, name='camp_delete'),
+  path('camp_show/<int:id>/', views.camp_show, name='camp_show'),
 ]
-  
