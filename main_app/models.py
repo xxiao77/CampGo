256ed0162ff9ebfcee6dbc1cc8fcac5161b8ea3d
xxiao77 <sophiaxiao717@gmail.com>
2020-02-10 09:59:43 -0800

    small change on models

--- main_app/models.py
+++ main_app/models.py
@@ -26,10 +26,3 @@ class Comment(models.Model):
 
   def get_absolute_url(self):
     return reverse('camp_show', kwargs={'campsite_id': self.campsite.id})
-
-class Favlist(models.Model):
-  campsite = models.ForeignKey(Campsite, on_delete=models.CASCADE)
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-  def __str__(self):
-    return self.content
